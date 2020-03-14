1b58be68978eb3cd4608a576acf4b1f7fd9fce0d
Tyler Croley <tjcroley@outlook.com>
2020-03-14 00:17:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,15 +15,26 @@ class Transfer
   end
   
 
-  def execute_transaction
-   total = @sender - @amount
-  
-   
-    #binding.pry 
-end
-    
-    
-    
+def execute_transaction
+    if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
 
-  
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
+    else
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
+    end
+  end
+
 end
